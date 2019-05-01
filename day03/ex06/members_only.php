f6ae0ba136f78807bd6933481a6682d9de14574e
Brandon norrey Bashirian <bbashiri@ox-a4.21-school.ru>
2019-05-01 19:10:28 +0300

    3

--- day03/ex06/members_only.php
+++ day03/ex06/members_only.php
@@ -1,18 +1,36 @@
 <?php
 
-if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitssponeys"){
+function ft_ok(){
 
-    echo ("<html><body>
-
-Hello Zaz<br/>
-<img src=\"../img/42.png\">
-
-</body></html>
-");
-
+    $img = file_get_contents("../img/42.png");
+    $img = base64_encode($img);
+    echo ("<html><body>");
+    echo ("Hello Zaz<br/>");
+    echo ("<img src='data:image/png;base64,$img'>");
+    echo ("</body></html>\n");
 
 }
-//header('WWW-Authenticate: Basic realm="My Realm"');
-//header('HTTP/1.0 401 Unauthorized');
-print_r($_SERVER);
+
+function ft_bad(){
+    echo ("<html><body>That area is accessible for members only</body></html>\n");
+}
+
+
+function main(){
+
+    if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitssponeys") {
+        ft_ok();
+        exit();
+    }
+    else {
+        header("WWW-Authenticate: Basic realm= ''Member area''");
+        header('HTTP/1.0 401 Unauthorized');
+        ft_bad();
+    }
+}
+
+main();
+
+//curl --user zaz:jaimelespetitssponeys localhost:8080
+//curl -v --user root:root localhost:8080
 ?>
